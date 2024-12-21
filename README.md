5e6d3dba0baf636ef8f2166566471a4cd6b96f93
RZAsadi <rezasadigm@gmail.com>
2024-12-21 15:58:47 +0330

    add setup.sh & README.md

---
+++ README.md
@@ -0,0 +1,9 @@
+# Install & Upgrade
+
+```
+bash <(wget -qO- https://raw.githubusercontent.com/RZAsadi/ir_tracking_post_api_node/refs/heads/master/setup.sh 2> /dev/null)
+```
+or
+```
+bash <(curl -Ls https://raw.githubusercontent.com/RZAsadi/ir_tracking_post_api_node/refs/heads/master/setup.sh)
+```
---
+++ setup.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+# Make sure the script is running as root
+if [ "$(id -u)" != "0" ]; then
+    echo -e "${BLUE}This script must be run as root.${NC}"
+    exit 1
+fi
+
+# Add Docker's official GPG key:
+sudo apt-get update
+sudo apt-get install ca-certificates curl
+sudo install -m 0755 -d /etc/apt/keyrings
+sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+sudo chmod a+r /etc/apt/keyrings/docker.asc
+
+# Add the repository to Apt sources:
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
+  $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update
+
+
+if [ -d "ir_tracking_post_api_node" ]; then
+    echo -e "${BLUE}Deleting existing xVpn directory...${NC}"
+    rm -rf xVpn
+fi
+
+# Clone the repository
+git clone https://github.com/RZAsadi/ir_tracking_post_api_node.git
+
+# Change to the project directory
+cd ir_tracking_post_api_node
+
+# Down the last container
+docker-compose down
+
+# Build & Run new Container
+docker-compose up --build -d
+
+echo -e "${BLUE}ir_tracking_post_api installed successfully.${NC}"
